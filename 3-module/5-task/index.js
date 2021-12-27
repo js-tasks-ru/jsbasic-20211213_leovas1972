1370e131654724e1bcfa411b666563e29e45d5f1
Vasilii <leovas1972@gmail.com>
2021-12-28 00:14:42 +0700

    3.5st task function getMinMax(str)

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = str.split(' ').map(item => (+item !== NaN) ? +item : '')
+    .filter(item => (item !== NaN) ? +item : '');
+  
+  let min = result[0], max = result[0];
+    
+  for (let i = 0; i < result.length; i++) {
+    if( result[i] < min ) min = result[i]; 
+    if( result[i] > max ) max = result[i]; 
+  }
+  
+  return {'min': min, 'max': max};
 }
